bbf87655e068b4141147338c128cef941fbb6923
Raihan Razi <raihan@192-168-1-100.tpgi.com.au>
2017-09-19 22:19:14 +1000

    first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log("hello from webpacks");
+import React from 'react';
+import { render } from 'react-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My Main Component</h1>
+      </div>
+    );
+  }
+}
+
+render(<App />, window.document.getElementById("app"));
